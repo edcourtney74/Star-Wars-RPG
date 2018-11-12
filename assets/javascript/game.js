351d1da7f38bbc678d0181f4ec70e8c0cb68859a
Ed Courtney <edcourtney74@gmail.com>
2018-11-12 15:14:08 -0500

    Logic working well. Need more CSS and moving people around.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,24 +2,42 @@
 // Array of possible hit points for each character
 var HPPossibles = [100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200];
 // Array of possible attack power value for each character
-var APPossibles = [4, 5, 6, 7, 8, 9, 10];
+var APPossibles = [5, 6, 7, 8, 9, 10, 11];
 // Array of possible counterattack power value for each character
 var CPPossibles = [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 // Variable for whether initial character has been chosen
 var isCharacterChosen = false;
 // Variable for chosen character HP
 var userHP;
+// // Variable for chosen character base AP
+var baseUserAP;
 // // Variable for chosen character AP
 var userAP;
 // Variable for defender HP
 var defenderHP;
 // Variable for defender CP
 var defenderCP;
-
+// Variable for wins - this won't display, but will be used to determine when all defenders have been defeated
+var wins;
 // GLOBAL FUNCTIONS
 // Reset game
 
 function resetGame() {
+    // Reset arrays, variables to original values
+    HPPossibles = [100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
+    APPossibles = [5, 6, 7, 8, 9, 10, 11];
+    CPPossibles = [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    isCharacterChosen = false;
+    wins = 0;
+    
+    // Disables attack button until characters are selected
+    $(".attack-btn").attr("disabled", true);
+
+    // Remove areas from previous games
+    $("#character-list").empty();
+    $("#enemies-left").empty();
+    $("#user-character").empty();
+    $("#defender-character").empty();
 
     // Character creation
     // Set random HP, attack and counterattack values for each character
@@ -74,11 +92,11 @@ function resetGame() {
     }
 }
 
-function selectUserCharacter() {
+function selectCharacters() {
     $(".start-character-image").on("click", function () {
         // If a character has not been chosen yet
         if (!isCharacterChosen) {
-
+            console.log(isCharacterChosen);
             // Remove start-character-image class
             $(this).removeClass("start-character-image");
 
@@ -87,24 +105,20 @@ function selectUserCharacter() {
 
             // Set isCharacterChosen so next choice will go to Defender area
             isCharacterChosen = true;
+            console.log(isCharacterChosen);
 
             // Retrieve HP & AP
-            defenderHP = $(this).attr("data-hp");
-            defenderHP = parseInt(defenderHP);
-            console.log(`This is your opponent's HP: ${defenderHP}`);
-            defenderCP = $(this).attr("data-cp")
-            defenderCP = parseInt(defenderCP);
-            console.log(`This is your character's CP: ${userCP}`);
+            userHP = $(this).attr("data-hp");
+            userHP = parseInt(userHP);
+            console.log(`This is your HP: ${userHP}`);
+            userAP = $(this).attr("data-ap")
+            userAP = parseInt(userAP);
+            baseUserAP = parseInt(userAP);
 
+            console.log(`This is your AP: ${userAP}`);
 
-        }
-    })
-}
-
-function selectDefender() {
-    $(".start-character-image").on("click", function () {
-        // If a character has been chosen yet
-        if (isCharacterChosen) {
+            // Need to move enemies elsewhere but can't figure it out
+        } else {
 
             // Remove start-character-image class
             $(this).removeClass("start-character-image");
@@ -112,33 +126,63 @@ function selectDefender() {
             // Move the character to the Defender area
             $("#defender-character").append(this);
 
+            // Add class to defender so it can be removed upon defeat
+            $(this).addClass("current-defender");
+
             // Retrieve HP & CP
-            userHP = $(this).attr("data-hp");
-            userHP = parseInt(userHP);
-            console.log(`This is your character's HP: ${userHP}`);
-            userAP = $(this).attr("data-ap")
-            userAP = parseInt(userAP);
-            console.log(`This is your character's AP: ${userAP}`);
+            defenderHP = $(this).attr("data-hp");
+            defenderHP = parseInt(defenderHP);
+            console.log(`This is your opponents's HP: ${defenderHP}`);
+            defenderCP = $(this).attr("data-cp")
+            defenderCP = parseInt(defenderCP);
+            console.log(`This is your opponents's CP: ${defenderCP}`);
+
+            // Enable attack button
+            $(".attack-btn").attr("disabled", false);
+
         }
     })
 }
 
+function attack () {
+    $(".attack-btn").on("click", function () {
+        userHP -= defenderCP;
+        defenderHP -= userAP;
+        console.log(`User HP: ${userHP}`)
+        console.log(`Defender HP: ${defenderHP}`)
+        userAP += baseUserAP;
+        console.log(`Your new AP: ${userAP}`);
+        
+        // Check to see if you lost
+        if (userHP <= 0) {
+                alert(`You lost. Try again.`);
+                // Disable attack button - game is over;
+                $(".attack-btn").attr("disabled", true);
+                // Reset game
+                startGame();
+
+        // Check to see if defender is out of HP
+        } else if (defenderHP <= 0) {
+                alert(`You beat this opponent!`)
+                // Removes defender from screen
+                $(".current-defender").empty();
+                // Locks attack button until another defender is selected
+                $(".attack-btn").attr("disabled", true);
+                // Adds 1 to win counter - not visible, only for tracking
+                wins ++;
+
+                // Check if defenders remain or the user beat them all
+                if (wins === 3) {
+                    alert("You've defeated them all. Way to go!");
+                    startGame();
+                }
+
+        }
+        
+        }) 
+    }
 
 
-
-
-
-
-
-// Game play 
-// Select user character
-// Move character to attack position
-// Move other characters to defense list
-// First battle
-// Battle function
-// Select defender
-// Move to defense position, remove from list position
-// Attack button clicked
 // Defender HP reduced by user HP, display on screen
 // User HP reduced by defender HP, display on screen
 // User HP attack value increases by base amount
@@ -154,14 +198,11 @@ function selectDefender() {
 // Start Game function to bundle other functions
 function startGame() {
     resetGame();
-    selectUserCharacter();
-    selectDefender();
+    selectCharacters();
+    attack();
 }
 
 // GAME PLAY============================================================
 $(document).ready(function () {
     startGame();
-
-
-
 })
